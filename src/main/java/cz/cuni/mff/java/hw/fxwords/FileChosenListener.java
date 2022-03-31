e210d892f8c9a7b6c05349fd3537621c9c6d0504
Jkuzz <honza.ku@gmail.com>
2022-03-31 17:08:09 +0200

    Implement wordcount

--- src/main/java/cz/cuni/mff/java/hw/fxwords/FileChosenListener.java
+++ src/main/java/cz/cuni/mff/java/hw/fxwords/FileChosenListener.java
@@ -2,14 +2,19 @@ package cz.cuni.mff.java.hw.fxwords;
 
 import javafx.event.ActionEvent;
 import javafx.scene.chart.BarChart;
-import javafx.scene.control.Label;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
 public record FileChosenListener(BarChart<String, Number> wordsChart,
-                                 Stage stage, Label message) {
+                                 Stage stage) {
 
     public void updateGraph(ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
@@ -19,6 +24,45 @@ public record FileChosenListener(BarChart<String, Number> wordsChart,
         if (chosenFile == null) {
             return;
         }
-        message.setText(chosenFile.getAbsolutePath());
+        String text;
+        try {
+            text = Files.readString(Path.of(chosenFile.getPath()));
+        } catch (IOException e) {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setContentText("Unable to read file");
+            a.show();
+            return;
+        }
+        LinkedHashMap<String, Integer> countedWords = countWords(text);
+        while (wordsChart.getData().size() > 0) {  // clear previous series
+            wordsChart.getData().remove(0);
+        }
+        XYChart.Series<String, Number> dataSeries = new XYChart.Series<>();
+
+        List<Map.Entry<String, Integer>> sortedWordCount = countedWords.entrySet()
+                .stream()
+                .sorted((a, b) -> b.getValue() - a.getValue())
+                .toList()
+                .subList(0, 25);
+        for(Map.Entry<String, Integer> entry: sortedWordCount) {
+            System.out.println(entry.getKey() + ":\t\t" + entry.getValue());
+            dataSeries.getData().add(new XYChart.Data<>(entry.getKey(), entry.getValue()));
+        }
+        dataSeries.setName(chosenFile.getAbsolutePath());
+        wordsChart.getData().add(dataSeries);
+
+    }
+
+    private LinkedHashMap<String, Integer> countWords(String text) {
+        LinkedHashMap<String, Integer> wordCounts = new LinkedHashMap<>();
+        for(String word: text.split("\\s+")) {
+            Integer oldCount = wordCounts.get(word);
+            if(oldCount == null) {
+                oldCount = 0;
+            }
+            wordCounts.put(word, oldCount + 1);
+        }
+
+        return wordCounts;
     }
 }
--- src/main/java/cz/cuni/mff/java/hw/fxwords/Main.java
+++ src/main/java/cz/cuni/mff/java/hw/fxwords/Main.java
@@ -21,34 +21,25 @@ import java.io.File;
 public class Main extends Application {
     private static BarChart<String, Number> makeChart() {
         CategoryAxis xAxis = new CategoryAxis();
-        xAxis.setLabel("Devices");
+        xAxis.setLabel("Words");
         NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("Visits");
-        BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
-        XYChart.Series<String, Number> dataSeries1 = new XYChart.Series<>();
-        dataSeries1.setName("2014");
-
-        dataSeries1.getData().add(new XYChart.Data<>("Desktop", 178));
-        dataSeries1.getData().add(new XYChart.Data<>("Phone"  , 65));
-        dataSeries1.getData().add(new XYChart.Data<>("Tablet"  , 23));
-        barChart.getData().add(dataSeries1);
-        return barChart;
+        yAxis.setLabel("Occurrences");
+        return new BarChart<>(xAxis, yAxis);
     }
 
 
     private Scene makeScene(Stage stage) {
-        Label message = new Label("Hello, JavaFX!");
         BarChart<String, Number> barChart = makeChart();
 
         Button fileChooserButton = new Button();
         fileChooserButton.setText("Select file");
         fileChooserButton.setMaxWidth(Double.MAX_VALUE);
-        FileChosenListener listener = new FileChosenListener(barChart, stage, message);
+        FileChosenListener listener = new FileChosenListener(barChart, stage);
         fileChooserButton.setOnAction(listener::updateGraph);
 
         BorderPane appPane = new BorderPane();
         appPane.setPadding(new Insets(20, 20, 20, 20));
-        VBox vbox = new VBox(fileChooserButton, message, barChart);
+        VBox vbox = new VBox(fileChooserButton, barChart);
         appPane.setCenter(vbox);
         return new Scene(appPane);
     }
